e444c3be6317ea12aaa3063c92df8c0298fd0853
cavalown <cavalown@gmail.com>
2023-02-09 00:55:43 +0800

    feat: add new task done

--- main.js
+++ main.js
@@ -1,29 +1,41 @@
 window.addEventListener("load", () => {
   const inputText = document.querySelector("#input-text");
   const addItem = document.querySelector(".add-btn");
-  const tasks = [];
+  // const tasks = [];
 
-  addItem.addEventListener("click", () => {
+
+  function addTask() {
     const newTask = inputText.value.trim()
     if (!newTask) {
       alert("Please text content!")
       return
     }
     const todos = document.querySelector(".todos");
-    tasks.push(newTask);
-    console.log("tasks:", tasks);
-    inputText.value = "";
-    todos.innerHTML += `<li class="todo">
-      <input type="checkbox">
+    const todoList = document.createElement('li')
+    todoList.classList.add('todo')
+    // tasks.push(newTask);
+    todoList.innerHTML += `<input type="checkbox">
       <p class="todo-content">${newTask}</p>
       <div>
           <button type="button" class="edit-btn">EDIT</button>
           <button type="button" class="remove-btn">DELETE</button>
-      </div>
-  </li>`
+      </div>`;
+    todos.appendChild(todoList);
+    inputText.value = "";
+  }
+
+  // 新增task: 點擊+
+  addItem.addEventListener("click", () => {
+    addTask();
+  })
+  // 新增task: enter
+  inputText.addEventListener("keypress", (event) => {
+    if (event.which === 13) addTask();
   })
 
+  // 編輯task
+  // 刪除task
+  // 完成task，並移至最下方。
   const editItem = document.querySelector(".edit-btn")
   const removeItem = document.querySelector(".remove-btn");
-  console.log('**', tasks);
 })
